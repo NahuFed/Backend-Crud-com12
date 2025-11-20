b57cfb2dd3c3c5e3ef019fee24165be9bdec4e3c
NahuFed <nahufed@users.noreply.github.com>
2025-11-20 14:41:34 -0300

    Agregar origen de Vercel a configuración CORS

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const app = express();
 
 //middlewares
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'], // Múltiples orígenes permitidos
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:3000',
+    'https://crud-com-12.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
